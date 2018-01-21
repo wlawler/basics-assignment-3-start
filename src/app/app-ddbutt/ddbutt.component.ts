f4f70b4938b5ef9a19cdfc0cfff9b0473e173244
wes lawler <wes.lawler2@gmail.com>
2018-01-20 18:24:26 -0600

    button works

---
+++ src/app/app-ddbutt/ddbutt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core' ;
+
+@Component({
+    selector: 'app-ddbutt',
+    templateUrl: './ddbutt.component.html', 
+
+
+}) 
+
+export class ddbuttComponent {
+
+}
