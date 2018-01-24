83475b4f4a7e20c60ff2898ae190a1bbddfd5120
wes lawler <wes.lawler2@gmail.com>
2018-01-24 17:08:02 -0600

    3rd task hw 2 complete

--- src/app/app-ddbutt/ddbutt.component.ts
+++ src/app/app-ddbutt/ddbutt.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core' ;
+import { Component, OnInit } from '@angular/core' ;
 
 @Component({
     selector: 'app-ddbutt',
@@ -7,6 +7,15 @@ import { Component } from '@angular/core' ;
 
 }) 
 
-export class ddbuttComponent {
+export class ddbuttComponent implements OnInit {
+    allowNewParagraph = false;
+     paragraphCreated = false; 
+
+     ngOnInit(){
+
+     }
+     onCreateParagraph() {
+         this.paragraphCreated = true;
+     }
 
 }
