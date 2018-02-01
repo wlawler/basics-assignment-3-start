325cfdb111acde4aa26bad583a3fe9cd4466a295
wes lawler <wes.lawler2@gmail.com>
2018-01-31 20:32:15 -0600

    task 4 semi complete

--- src/app/app-ddbutt/ddbutt.component.ts
+++ src/app/app-ddbutt/ddbutt.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core' ;
+import { ViewChild } from '@angular/core';
+
 
 @Component({
     selector: 'app-ddbutt',
@@ -9,13 +11,26 @@ import { Component, OnInit } from '@angular/core' ;
 
 export class ddbuttComponent implements OnInit {
     allowNewParagraph = false;
-     paragraphCreated = false; 
+     paragraphCreated = false;
+     buttonclicks : MouseEvent[]= [];
+      @ViewChild ("cat")ccc;
+
+      
 
      ngOnInit(){
 
      }
-     onCreateParagraph() {
-         this.paragraphCreated = true;
+    
+     onCreateParagraph(click:MouseEvent)  {
+        this.buttonclicks.push (click) ; 
+        this.paragraphCreated = true;
+         console.log(this.ccc);
+         
      }
+     getColor() {
+      return this.buttonclicks.length >= 5 ? 'blue' : 'white';
+     }
+     
+
 
 }
